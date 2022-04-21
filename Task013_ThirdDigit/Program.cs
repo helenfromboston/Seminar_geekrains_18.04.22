71476e062cf86d0c93b912c5803a89879e098f21
helenfromboston <helenfromboston@gmail.com>
2022-04-21 19:24:48 +0300

    Added tasks 013, 015

---
+++ Task013_ThirdDigit/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100) Console.Write($"В числе {number} третьей цифры нет");
+else if (number <= 999) Console.Write($"Третья цифра – {number % 10}");
+else
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    Console.Write($"Третья цифра – {number % 10}");
+}
---
+++ Task015_Weekend/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите цифру дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 1 && number <= 5) Console.Write("Нет");
+else if (number == 6 || number == 7) Console.Write("Да");
+else Console.Write("Введена недействительная цифра");
